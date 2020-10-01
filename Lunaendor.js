fad5dac28a704e8639fb908e843ce8cdecafc4ac
Salarcve <63760494+Salarcve@users.noreply.github.com>
2020-09-30 19:19:56 -0500

    Taller 1

---
+++ Lunaendor.js
@@ -0,0 +1,5 @@
+
+Temperatura = (temperaturamax, Temperaturaminima) =>{
+    return (((Math.max(temperaturamax )) + (Math.min(Temperaturaminima))))/2;
+}
+console.log(Temperatura(91,20));
---
+++ codigos.js
@@ -0,0 +1,4 @@
+let tripulantes =  "ARQ2555: Sara Bel-Sun ,ARQ2556: Nodin Chavdri, ARQ2557: Finn";
+let nave =  tripulantes.split(",");
+console.log(nave);
+
