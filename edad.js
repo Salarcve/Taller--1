693d277ad89b71f752b58d01648aa78e2e511b5d
Salarcve <63760494+Salarcve@users.noreply.github.com>
2020-10-21 20:48:28 -0500

    Edadjugadores

---
+++ edad.js
@@ -0,0 +1,9 @@
+let calcularedad = new Date('12/11/1997');
+let edad = new Date;
+let años = edad.getFullYear() - calcularedad.getFullYear();
+
+calcularedad.setFullYear(edad.getFullYear);
+ if(edad < calcularedad){
+     años;
+ }
+ console.log(años);
